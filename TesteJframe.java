6ac67236533b95eb07fda48a5c8af9df385a93a4
GuiLMarques22 <120430912+GuiLMarques22@users.noreply.github.com>
2023-03-23 21:17:35 -0300

    Add files via upload

---
+++ TesteJframe.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+public class TesteJframe {
+
+        public static void main(String[] args) {
+            TesteJframe janela = new JFrame("Meu primeiro frame em Java");
+                    janela.setVisible(true);
+        }
+    }
+
